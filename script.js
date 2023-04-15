4138c7be0d13c15a19a41a6809b5d4fa3e2d1679
Akorede Olawole <akoredeolawole@gmail.com>
2023-04-15 21:34:27 +0100

    implement about us page

---
+++ script.js
@@ -0,0 +1,32 @@
+ // VARIABLES
+
+ // This is a program to calculate two numbers
+// I'll declare a varaible first number 
+ // I'll declare a varaible second number
+
+ /* This is a program to calculate two numbers
+    I'll declare a varaible first number 
+    I'll declare a varaible second number
+ */
+
+// var 
+// let
+// const myName = "Olawole"
+// myName = "Oscar"
+
+// var myName = "Olawole"
+// var myName = "Oscar"
+
+// let myOtherName = "Mubarak"
+// let myOtherName = "Korede"
+
+// let firstNumber = 20
+// let secondNumber = 40
+
+// firstNumber = 80
+
+// console.log(myName)
+// console.log(myOtherName)
+// console.log(firstNumber + secondNumber)
+ 
+// learning git and github
